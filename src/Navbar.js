50e602ec0cbadfab2002cfc2f97852a376190aac
Nurhan Sayed <nurhansaydd@gmail.com>
2022-02-22 16:04:05 +0200

    task4

--- src/Navbar.js
+++ src/Navbar.js
@@ -13,7 +13,6 @@ export default function Navbar(){
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
-
   <div className="collapse navbar-collapse" id="navbarNav">
     <ul className="navbar-nav">
       <li className="nav-item active">
